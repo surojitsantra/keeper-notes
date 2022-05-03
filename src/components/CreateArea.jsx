810f05eaf3e905f61f5a179befb8ec83d7c6092e
Surojit <surojitsantra97@gmail.com>
2022-05-03 12:40:39 +0530

    Restrict showing content lengths

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,33 +1,50 @@
 import React from "react";
 
 function CreateArea(props) {
+  const [note, setNote] = React.useState({
+    title: "",
+    content: "",
+  });
 
-    const[note, setNote] = React.useState({
-        title: "",
-        content: ""
-    })
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
 
-    function handleChange(event) {
-      const {name, value} = event.target;
-      setNote((prevNote) => {
-        return({
-          ...prevNote,
-          [name]: value
-        });
-      });
-
-    }
-
-    function submitNote(event) {
+  function submitNote(event) {
+    if (note.title.trim() !== "") {
       props.addNote(note);
+      setNote({
+        title: "",
+        content: "",
+      });
       event.preventDefault();
     }
+  }
 
   return (
     <div>
       <form>
-        <input name="title" onChange={handleChange} value = {note.title} placeholder="Title"/>
-        <textarea name="content" onChange={handleChange} value = {note.content} placeholder="Take a note..." rows="3" />
+        <input
+          name="title"
+          required={true}
+          maxLength="30"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button onClick={submitNote}>Add</button>
       </form>
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 
 function Note(props) {
+  const [showTitle, setShowTitle] = React.useState(true);
+  const [showContent, setShowContent] = React.useState(true);
 
   function deleteNote(event) {
     props.deleteNote(props.id);
     event.preventDefault();
   }
 
-    return (<div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
+  return (
+    <div className="note">
+      <h1
+        onClick={() => {
+          setShowTitle((prevState) => !prevState);
+        }}
+      >
+        {props.title.length > 15 && showTitle
+          ? props.title.substring(0, 15) + "..."
+          : props.title}
+      </h1>
+      <p
+        onClick={() => {
+          setShowContent((prevState) => !prevState);
+        }}
+      >
+        {props.content.length > 25 && showContent
+          ? props.content.substring(0, 25) + "..."
+          : props.content}
+      </p>
       <button onClick={deleteNote}>Delete</button>
-    </div>);
+    </div>
+  );
 }
 
 export default Note;
